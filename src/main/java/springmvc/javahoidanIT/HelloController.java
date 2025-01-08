16b4d24bc55475de758e43381d1496a456fc95e6
dtphu03 <145108284+dtphu03@users.noreply.github.com>
2025-01-08 11:09:11 +0700

    commit

---
+++ src/main/java/springmvc/javahoidanIT/HelloController.java
@@ -0,0 +1,14 @@
+package spring_mvc.java_hoidanIT;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World + Spring";
+    }
+
+}
--- src/main/java/springmvc/javahoidanIT/JavaHoidanItApplication.java
+++ src/main/java/springmvc/javahoidanIT/JavaHoidanItApplication.java
@@ -1,4 +1,4 @@
-package SpringMVC01.JavaHoidanIT;
+package spring_mvc.java_hoidanIT;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
