83fbfb40049e19d37a04ed12bfc1fc535d2d807b
dtphu03 <145108284+dtphu03@users.noreply.github.com>
2025-01-08 10:11:55 +0700

    first commit

---
+++ src/main/java/SpringMVC01/JavaHoidanIT/JavaHoidanItApplication.java
@@ -0,0 +1,13 @@
+package SpringMVC01.JavaHoidanIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHoidanItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHoidanItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SpringMVC_01/Java_HoidanIT/JavaHoidanItApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringMVC_01.Java_HoidanIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHoidanItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
